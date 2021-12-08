2c22ab75d05a44e08e6bdcfa8b93381a0ec26df0
Jakub D <contactme@jakubdobos.eu>
2021-12-08 16:17:36 +0100

    toPrestige

--- src/HUD/MatchBar/WinIndicator.tsx
+++ src/HUD/MatchBar/WinIndicator.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Team } from 'csgogsi-socket';
 import TeamLogo from './TeamLogo';
-import ucLogo from '../../assets/ucClassic.png';
+import ucLogo from '../../assets/ucPrestige.png';
 
 
 export default class WinAnnouncement extends React.Component<{ team: Team | null, show: boolean }> {
@@ -11,7 +11,7 @@ export default class WinAnnouncement extends React.Component<{ team: Team | null
         return <div className={`win_text ${show ? 'show' : ''} ${team.orientation} ${team.side}`}>
             <div className="winnerLogo"><TeamLogo team={team} /></div>
             <span><div className="teamName">{team.name}</div></span><span>Vyhráva Kolo!</span>
-            <div className="ucLogo"><img src={ucLogo}/></div>
+            <div className="ucLogo"><img src={ucLogo} alt="Logo"/></div>
             </div>
     }
 }
